be5efe6167b10edd5a10cd549485f9c110aff5a0
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-07-19 19:08:50 +0100

    update the user model to specify related_name

--- App/models.py
+++ App/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
 class User(AbstractUser):
@@ -6,6 +6,21 @@ class User(AbstractUser):
     gender = models.CharField(max_length=10, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name='users_in_group',  # Updated related_name
+        blank=True,
+        help_text='The groups this user belongs to.',
+        verbose_name='groups'
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='users_with_permission',  # Updated related_name
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions'
+    )
 
 class HealthMetrics(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
