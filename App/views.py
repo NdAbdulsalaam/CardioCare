4181f6b1d11a4f83d027b5859bb7f89112027d52
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-07-19 18:30:32 +0100

    create basic view for each model

--- App/views.py
+++ App/views.py
@@ -1,5 +1,27 @@
-# core/views.py
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User, HealthMetrics, RiskAssessment, Recommendations, Reminders, ProgressTracking
+from .serializers import UserSerializer, HealthMetricsSerializer, RiskAssessmentSerializer, RecommendationsSerializer, RemindersSerializer, ProgressTrackingSerializer
 
-def index(request):
-    return render(request, 'index.html')
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class HealthMetricsViewSet(viewsets.ModelViewSet):
+    queryset = HealthMetrics.objects.all()
+    serializer_class = HealthMetricsSerializer
+
+class RiskAssessmentViewSet(viewsets.ModelViewSet):
+    queryset = RiskAssessment.objects.all()
+    serializer_class = RiskAssessmentSerializer
+
+class RecommendationsViewSet(viewsets.ModelViewSet):
+    queryset = Recommendations.objects.all()
+    serializer_class = RecommendationsSerializer
+
+class RemindersViewSet(viewsets.ModelViewSet):
+    queryset = Reminders.objects.all()
+    serializer_class = RemindersSerializer
+
+class ProgressTrackingViewSet(viewsets.ModelViewSet):
+    queryset = ProgressTracking.objects.all()
+    serializer_class = ProgressTrackingSerializer
