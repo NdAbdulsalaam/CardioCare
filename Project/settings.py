150f0bc94c9dac1583f45711e18b317aba7a7477
NdAbdulsalaam <ndabdulsalaam@gmail.com>
2024-11-19 21:55:40 +0100

    test if arrangement of allowed host in the settings file affect

--- Project/settings.py
+++ Project/settings.py
@@ -28,7 +28,7 @@ OAUTH_CLIENT_ID = os.getenv('OAUTH_CLIENT_ID')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['.vercel.app', '*']
+ALLOWED_HOSTS = ['*', '.vercel.app']
 
 # Application definition
 
